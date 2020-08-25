b0be0e180026cfff05a6fbf72d4ea96a9c0d50f3
rbbtz <robbetz@gmail.com>
2020-08-25 14:57:36 -0400

    complete

--- SwitchStatements/Program.cs
+++ SwitchStatements/Program.cs
@@ -6,11 +6,26 @@ namespace SwitchStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your favorite school subject?");
 
+            var subject = Console.ReadLine(); // evaluate to a string
 
+                switch (subject.ToLower())
+            {
+                    case "math":
+                        Console.WriteLine("Math is a tough subject!");
+                        break;
+                    case "science":
+                        Console.WriteLine("Science is a tough subject!");
+                        break;
+                    case "english":
+                        Console.WriteLine("Math is an interesting subject!");
+                        break;
+                    default:
+                        Console.WriteLine($"Oh wow! I haven't taken that subject before. {subject} sounds fun!");
+                        break;
 
-
+            }
         }
     }
 }
